11f0c4bf3dfd0e1df80f967455c79319cd7f3c85
/ A N K I T / <xnkitk69@gmail.com>
2024-01-27 22:12:22 +0530

    Create index.js

---
+++ scripts /index.js
@@ -0,0 +1,55 @@
+const numberOfChats = 10;
+let fakeChats = [];
+// generate an array of fake chats to show in app
+for (let i = 1; i < numberOfChats; i++) {
+    fakeChats.push(chatGenerator(i))
+}
+
+// this is the signed-in user object
+const authedUser = {
+    id: '12',
+    name: "Ankit Kumar",
+    username: "xnkit69",
+    online: true,
+    lastSeen: "Today",
+    avatar: "https://radare.arzfun.com/api/tg/photo?id=AgACAgUAAxkBAAIIRWWWiU78nesTwlvO_5anPyIHYF7IAAIEujEbPF25VG-UELR1cjq5AQADAgADeQADNAQ"
+};
+
+// create instance of ChatApp,
+// this is the line that run application
+const app = new ChatApp("chat-web-app");
+app.signin(authedUser);
+
+// add all generated chats to app one-by-one
+fakeChats.map(fc => app.addChat(fc));
+
+
+// below code is just for simulating message receive
+// here we send 100 messages in different times ro app
+let fakeMsgCounter = 100;
+const interval = setInterval(() => {
+
+    if (--fakeMsgCounter === 0) {
+        clearInterval(interval);
+        return;
+    }
+
+    setTimeout(() => {
+        const fakeSender = fakeChats[randomNumber(numberOfChats, 1)];
+        if (!fakeSender)
+            return;
+
+        // flag with a 20% probability
+        const randomFlag = Math.random() > .8;
+
+        app.newMessage({
+            text: getRandomText(Math.random() > .5),
+            sender: randomFlag ? authedUser.id : fakeSender.id,
+            time: new Date(),
+            toChat: randomFlag ? fakeSender.id : authedUser.id
+        });
+
+        // new message sending time can be dynamic, between 1s and 5s
+    }, randomNumber(1000, 5000))
+
+}, 1500);
